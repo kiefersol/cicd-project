d3a969f0b853485104f6fcfb2f5b045554459af6
kiefersol <kiefer.sol96@gmail.com>
2025-03-31 01:14:51 +0000

    grafana create dashboard

--- automation/ansible/files/k8s_monitoring/create_lb.sh
+++ automation/ansible/files/k8s_monitoring/create_lb.sh
@@ -9,7 +9,7 @@ cd $BASEDIR
 
 #grafana config파일에 json파일 업로드
 cd $BASEDIR/json
-kubectl create configmap istio-services-grafana-dashboards --namespace=istio-system  --from-file=./prometheus_node_exporter.json --from-file=./k8s_all_cluster_monitoring.json --from-file=./k8s_cluster_summary.json --from-file=./loki_kubernetes_logs.json --from-file=./cilium_agent_metrics.json --from-file=./hubble_metrics.json
+kubectl create configmap istio-services-grafana-dashboards --namespace=istio-system  --from-file=./prometheus_node_exporter.json --from-file=./k8s_all_cluster_monitoring.json --from-file=./k8s_cluster_summary.json --from-file=./loki_kubernetes_logs.json --from-file=./cilium_agent_metrics.json --from-file=./hubble_metrics.json --from-file=./sol_namespace.json
 
 kubectl apply -f $BASEDIR/yaml_lb/
 
